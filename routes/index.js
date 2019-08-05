46beb172e2e1ac7dab85301c1e202e9c5ff8800d
Eric Paul A. Jaucian <JaucianE@tgtmgmt.local>
2019-08-05 10:40:10 +0800

    Stop ate 1:08 of video

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,10 @@ let landing = require("../controller/landing");
 /* GET home page. */
 router.get("/", landing.get_landing);
 router.post("/", landing.submit_lead);
+router.get("/leads", landing.show_leads);
+router.get("/lead/:lead_id", landing.show_lead);
+router.get("/lead/:lead_id/edit", landing.show_edit_lead);
+router.post("/lead/:lead_id/edit", landing.edit_lead);
+router.post("/lead/:lead_id/delete", landing.delete_lead);
+
 module.exports = router;
